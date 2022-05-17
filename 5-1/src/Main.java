1ac6cfdd0d3a7e08338d59608d57f49cd50a5bf8
nz24k <102730462+nz24k@users.noreply.github.com>
2022-05-17 21:20:53 +0900

    5章チェックテスト

---
+++ 5-1/src/Main.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		{
+//      問① 下記の配列はString型しか格納できないように修正してください。
+//		   現状は全てのクラスの継承元であるObject型を指定しているので、Integerもintも格納可能になっています。
+//	       List<Object> array = new ArrayList<>();
+//	    <以下記述>
+	       List<String> array = new ArrayList<String>();
+//      問② 前から二つ目の要素を"bar"にしましょう。
+//	　<以下記述>
+	       array.add("bar");
+	       array.add("foo");
+//      問③ fooが格納されているインデックスを出力してください。
+//	　<以下記述>
+	       System.out.println("fooのインデックス:" + array.get(1)); 
+		}
+
+		{
+//      問④ キーがString型、バリューがObject型の要素を三つ格納しましょう。
+		Map<String, Object> map = new HashMap<>();
+//	　<以下記述>
+		map.put("address", "///");
+		map.put("name", "Kosuke");
+		map.put("age", 29);
+
+//      問⑤上記で格納したキーを繰り返し文で出力しましょう。
+//	    <以下記述>
+		for (String key: map.keySet()) {
+			System.out.println("key:" + key);
+		}
+		
+
+//      問⑥上記で格納したバリューを繰り返し文で出力しましょう。
+//	    <以下記述>
+		for (Object value: map.values()) {
+			System.out.println("value:" + value);
+		}
+
+		}
+		{
+//      問⑦ 配列arrayDateに今の年・月・日を入れてください。
+			Calendar date = Calendar.getInstance();
+           
+//	    <以下記述>
+			int [] arrayDate = {date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DATE)};
+
+
+//      問⑧ 配列arrayDateをList型に変更しましょう。
+//	    <以下記述>
+           List<Integer>  arrayDate1= new ArrayList<Integer>();
+           for (int i = 0; i < arrayDate.length; i++) {
+        	   arrayDate1.add(arrayDate[i]);
+           }
+           
+//      問⑨ 上記で格納した要素を繰り返しで出力しましょう。
+//	    <以下記述>
+           for (int i = 0; i < arrayDate1.size(); i++) {
+        	   System.out.println(arrayDate1.get(i));
+           }
+
+		}
+	}
+
+}
